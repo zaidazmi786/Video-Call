905be2ec0d45d6498c17bc1732c2aad6b2a38149
zaidazmi07 <zaidazmi7773@gmail.com>
2026-09-19 10:26:08 +0530

    deploy

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -12,7 +12,7 @@ import ScreenShareIcon from '@mui/icons-material/ScreenShare';
 import StopScreenShareIcon from '@mui/icons-material/StopScreenShare'
 import ChatIcon from '@mui/icons-material/Chat'
 import { Route, useNavigate } from "react-router-dom";
-
+import server from "../environment";
 
 const server_url = server;
 var Connections = {};
